83e32f9cccf39ace315e67655b1aefa9d5a173a0
Adarsh <adarshmoradiya001@gmail.com>
2022-01-06 14:25:41 +0530

    updated

--- sorting/merge_sort.js
+++ sorting/merge_sort.js
@@ -1,16 +1,11 @@
-var a = [5,24,4,85,1,33,15,78,55];
+var a = [5,24,4,85,1,33,15,78,55,35];
 // var start = 0, end = a.length-1;
 a=merge_sort(a);
 console.log(a);
 
 function merge_sort(a){
-    // if(start < end){
-    //     var mid = (start+end)/2;    
-    //     merge_sort(a,start,mid);
-    //     merge_sort(a,mid+1,end);
-    // }
-    // merge(a,start,mid,end);
-    const mid = a.length/2;
+    const mid = Math.floor(a.length/2);
+    console.log(mid);
     if(a.length < 2)    return a;
     const tmp = a.splice(0,mid);
     return merge(merge_sort(tmp),merge_sort(a));
@@ -22,21 +17,4 @@ function merge(left,right){
         else  ar.push(right.shift());
     }
     return [...ar,...left,...right];
-    // var p=start,q=mid+1;
-    // var ar= new Array(end-start+1);
-    // // console.log(ar.length);
-    // var k=0;
-    // for(var i=start;i<=end;i++){
-    //     if(p>mid)   
-    //         ar.push(a[q++]);
-    //     else if(q>end)   
-    //         ar.push(a[p++]);
-    //     else if(a[p]>a[q])   
-    //         ar.push(a[q++]);
-    //     else   
-    //         ar.push(a[p++]);
-    // }
-    // for(var i=0;i<ar.length;i++){
-    //     a[start++] = ar[i];
-    // }
 }
