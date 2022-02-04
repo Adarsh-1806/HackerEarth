298fdfc58e634e2eda49c6fa3a6c28b473c9e917
Adarsh <adarshmoradiya001@gmail.com>
2022-02-04 16:35:45 +0530

    complete

--- Solidity/Exercise/Bank.sol
+++ Solidity/Exercise/Bank.sol
@@ -4,38 +4,36 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Bank{
     address private owner;
-    address[] public accounts;
-    uint public bankbalance;
-    // address payable reciever = payable(msg.sender);
-    mapping (address=>uint) accountSheet;
+    uint public totalBalance;
+    mapping (address=>uint) accHolders;
+
     constructor(){
         owner = msg.sender;
-        bankbalance=0;
     }
-    function showowner() public view returns(address){
-        return owner;
-    }
-    //Function to Deposit Ether For account => this contract
+
+    error NotEnoughFunds(uint requisted, uint available);
+
+    //Function to Transfer Ether For account => this contract
     function DepositEther() public payable{
-        if(msg.value>1 ether){
-            accounts.push(msg.sender);
-            bankbalance += msg.value;
-            accountSheet[msg.sender] = msg.value;
-        }
+        accHolders[msg.sender] += msg.value;
+        totalBalance += msg.value;
     }
 
     //Function to Transfer Ether For this Contract => account
-    function WithdrawEther(address payable _reciever,uint _amount) public{
-       if(_amount >= accountSheet[_reciever]){
-           _reciever.transfer(_amount);
-           bankbalance -= _amount;
-           accountSheet[_reciever] -= _amount;
-       }
+    function WithdrawEther(uint _amount) public payable{
+        uint money = _amount*(10**15);
+        if(accHolders[msg.sender] < money){
+            revert NotEnoughFunds(money, accHolders[msg.sender]);
+        }
+            payable(msg.sender).transfer(money);
+            accHolders[msg.sender] -= money;
+        
+    }
+    
+    function acc() public view returns(address){
+        return msg.sender;
     }
     function CheckBalance() public view returns(uint){
-        return address(this).balance;
-    }
-    function balanceof(address _account) public view returns(uint){
-        return accountSheet[_account];
+        return accHolders[msg.sender];
     }
 }
